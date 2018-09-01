fa8029750a55123dad48f691f0386725d1a73e29
Filipe Maciel <devfilsk@gmail.com>
2018-09-01 14:33:22 -0300

    Configurações iniciais da estrutura

---
+++ main.js
@@ -0,0 +1,9 @@
+// const links = Array.from(document.querySelectorAll('.links-nav ul>li'));
+// const secoes = document.querySelectorAll('.section');
+
+
+// function abrirSection() {
+//     var name = location.hash.substring(1);
+//     var secao = document.getElementById(name);
+//     secao.style.display = "block";
+// }
