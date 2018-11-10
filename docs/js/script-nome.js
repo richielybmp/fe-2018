d425ad0fcca53779ca4f4b95bbd087fcf3b719ab
Filipe Maciel <devfilsk@gmail.com>
2018-11-10 15:53:25 -0200

    cadastro de nomes e uso completo

--- docs/js/script-nome.js
+++ docs/js/script-nome.js
@@ -33,7 +33,7 @@ function acaoBotaoSalvarNome(el) {
     if (!inconsistente) {
         //$(el).closest(".form-identificadores").find(".identificador input,select").prop("disabled", true);
         //el.setAttribute("salvou", true);
-        $(".caixa-nome.opcional").hide()
+        //$(".caixa-nome.opcional").hide()
         //$("#btnNovoIdentificador")[0].style.display = "block";
         $("#regiao-nomes").hide();
 
@@ -41,6 +41,7 @@ function acaoBotaoSalvarNome(el) {
     }
 }
 
+let id_nome = 1;
 function adicionarNomeNaTabela(){
     var bodytabelaIdentificadores = $("#tabelaNomes tbody")[0];
     var bodytabelaIdentificadoresVinculos = $("#tabelaIdentificadoresVinculos tbody")[0];
@@ -54,27 +55,65 @@ function adicionarNomeNaTabela(){
 
     let nome = $("#nome").val();
     let sobrenome = $("#sobrenome").val();
+    let uso = $("#uso-nome").val();
+    let arrayUso = {
+        1: 'Relatório',
+        2: 'Nome de recém-nascido',
+        3: 'Nome profissional ou comercial',
+        4: 'Nome de solteiro, nome de nascimento ou nome original',
+        5: 'Nome registrado (nome legal)',
+        8: 'Outro nome (alias)'
+    };
 
 
     var elTr = criarComponenteHtmlDinamico({ tag: "tr", id: nome + sobrenome });
 
-    elTr.appendChild(criarComponenteHtmlDinamico({ tag: "td", innerHTML: "ID-" + /*(ehVinculo ? "-V" : "-P" )*/ + id++ }));
+    elTr.appendChild(criarComponenteHtmlDinamico({ tag: "td", innerHTML: "ID-" + /*(ehVinculo ? "-V" : "-P" )*/ + id_nome++ }));
     elTr.appendChild(criarComponenteHtmlDinamico({ tag: "td", innerHTML: nome }));
     elTr.appendChild(criarComponenteHtmlDinamico({ tag: "td", innerHTML: sobrenome }));
-    elTr.appendChild(criarComponenteHtmlDinamico({ tag: "td", innerHTML: '' }));
+    elTr.appendChild(criarComponenteHtmlDinamico({ tag: "td", innerHTML: arrayUso[uso] }));
     elTr.appendChild(criarComponenteHtmlDinamico({ tag: "td", innerHTML: '<input type="radio" name="preferido" id="">' }));
 
     bodytabelaIdentificadores.appendChild(elTr);
     $("#tabelaNomes").show();
+    if(id_nome < 9)
     $("#btnNovoNome")[0].style.display = "block";
 
 }
 function mostrarNovoCadastroDeNome(el){
     var options = {
         el: el,
-        form: $(".form-identificadores"),
+        form: $(".form-nomes"),
         classForm: "nome",
         regiao: $("#regiao-nomes")
     }
+    $(".caixa-nome.opcional").fadeIn("fast");
     mostrarNovoCadastroForm(options);
+}
+
+/************* Representação ************/
+function adicionarRepresentacaoNome(el) {
+    let grupoUltimoFilho = $($(".container-representacao-nome")[0].children).last();
+    if(grupoUltimoFilho.find('input').val() != ""){
+        grupoUltimoFilho.clone().appendTo('.container-representacao-nome')
+        $($(".container-representacao-nome")[0]
+            .children)
+            .last()
+            .find('input')
+            .val("")
+            .focus()
+    }else{
+        $($($(".container-representacao-nome")[0].children).last().find('input')).css("border-color", "red")
+        return;
+    }
+    $(grupoUltimoFilho).find('input').css("border-color", "#ced4da")
+    $($($(".container-representacao-nome")[0].children).last().find('input')).css("border-color", "#ced4da")
+
+}
+function removerRepresentacaoNome(el) {
+    //$("#form-nomes").find(".campo-representacao").remove($($(".container-representacao-nome")[0].children).last())
+    if($(".container-representacao-nome")[0].children.length > 1){
+        $($(".container-representacao-nome")[0].children).last().remove()
+    }
+
 }
